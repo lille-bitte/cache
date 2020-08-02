c62662a77262260d50e05d4c2edbcaaa0ccee158
plvhx <gandung@slackerc0de.us>
2020-08-02 08:17:11 +0700

    Adding src/CacheAwareAdapterInterface.php

---
+++ src/CacheAwareAdapterInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LilleBitte\Cache;
+
+use Psr\Cache\CacheItemPoolInterface;
+
+/**
+ * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ */
+interface CacheAwareAdapterInterface extends CacheItemPoolInterface
+{
+}
