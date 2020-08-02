43e4041710e6b27a2255a3bdc0ff71cb10f39b18
plvhx <gandung@slackerc0de.us>
2020-08-02 16:03:07 +0700

    Adding tests/Integration/CachePool/Adapter/ApcCachePoolIntegrationTest.php

---
+++ tests/Integration/CachePool/Adapter/ApcCachePoolIntegrationTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LilleBitte\Cache\Tests\Integration\CachePool\Adapter;
+
+use Cache\IntegrationTests\CachePoolTest;
+use LilleBitte\Cache\Adapter\ApcCachePool;
+
+/**
+ * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ */
+class ApcCachePoolIntegrationTest extends CachePoolTest
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function createCachePool()
+	{
+		return new CachePoolTest();
+	}
+}
