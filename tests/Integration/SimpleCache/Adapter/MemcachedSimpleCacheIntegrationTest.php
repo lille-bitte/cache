d1c4bf678ba85e42c7deb12f18ac4eb23a47aa2a
plvhx <gandung@slackerc0de.us>
2020-08-05 23:51:01 +0700

    Adding tests/Integration/SimpleCache/Adapter/MemcachedSimpleCacheIntegrationTest.php

---
+++ tests/Integration/SimpleCache/Adapter/MemcachedSimpleCacheIntegrationTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LilleBitte\Cache\Tests\Integration\SimpleCache\Adapter;
+
+use Memcached;
+use Cache\IntegrationTests\SimpleCacheTest;
+use LilleBitte\Cache\Adapter\MemcachedCachePool;
+use LilleBitte\Cache\Bridge\SimpleCache\SimpleCacheBridge;
+
+/**
+ * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ */
+class MemcachedSimpleCacheIntegrationTest extends SimpleCacheTest
+{
+	/**
+	 * Create cache pool object.
+	 *
+	 * @return CacheItemPoolInterface
+	 */
+	private function createCachePool()
+	{
+		$memcached = new Memcached();
+		$memcached->addServer('localhost');
+		return new MemcachedCachePool($memcached);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function createCachePool()
+	{
+		return new SimpleCacheBridge($this->createCachePool());
+	}
+}
