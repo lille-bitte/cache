5dcb7631506f6822f6836a9bef3502e00f5ae7cc
plvhx <gandung@slackerc0de.us>
2020-08-06 00:05:09 +0700

    Refactoring src/Adapter/MemcacheCachePool.php

--- src/Adapter/MemcacheCachePool.php
+++ src/Adapter/MemcacheCachePool.php
@@ -16,56 +16,56 @@ use function unserialize;
  */
 class MemcacheCachePool extends CacheItemPool
 {
-	/**
-	 * @var Memcache
-	 */
-	private $cache;
+    /**
+     * @var Memcache
+     */
+    private $cache;
 
-	public function __construct(Memcache $cache)
-	{
-		$this->cache = $cache;
-	}
+    public function __construct(Memcache $cache)
+    {
+        $this->cache = $cache;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function storeItemToCache(CacheItemInterface $item, $ttl)
-	{
-		return $this->cache->set(
-			$item->getKey(),
-			serialize([$item->get(), $item->getExpirationTimestamp()]),
-			0,
-			$ttl ?? 0
-		);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function storeItemToCache(CacheItemInterface $item, $ttl)
+    {
+        return $this->cache->set(
+            $item->getKey(),
+            serialize([$item->get(), $item->getExpirationTimestamp()]),
+            0,
+            $ttl ?? 0
+        );
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function fetchItemFromCache($key)
-	{
-		if (false === ($cachedItem = $this->cache->get($key))) {
-			return [false, null, null];
-		}
+    /**
+     * {@inheritdoc}
+     */
+    protected function fetchItemFromCache($key)
+    {
+        if (false === ($cachedItem = $this->cache->get($key))) {
+            return [false, null, null];
+        }
 
-		$data = unserialize($cachedItem);
-		return [true, $data[0], $data[1]];
-	}
+        $data = unserialize($cachedItem);
+        return [true, $data[0], $data[1]];
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function removeAllItemFromCache()
-	{
-		return $this->cache->flush();
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function removeAllItemFromCache()
+    {
+        return $this->cache->flush();
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function removeItemFromCache($key)
-	{
-		$this->cache->delete($key);
-		return true;
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function removeItemFromCache($key)
+    {
+        $this->cache->delete($key);
+        return true;
+    }
 }
