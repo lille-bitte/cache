a4393dfc95ae84840293028cf565307d33f2821a
plvhx <gandung@slackerc0de.us>
2020-08-06 00:04:28 +0700

    Refactoring src/Adapter/ApcuCachePool.php

--- src/Adapter/ApcuCachePool.php
+++ src/Adapter/ApcuCachePool.php
@@ -19,48 +19,48 @@ use function unserialize;
  */
 class ApcuCachePool extends CacheItemPool
 {
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function storeItemToCache(CacheItemInterface $item, $ttl)
-	{
-		return apcu_store(
-			$item->getKey(),
-			serialize([$item->get(), $item->getExpirationTimestamp()]),
-			$ttl ?? 0
-		);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function storeItemToCache(CacheItemInterface $item, $ttl)
+    {
+        return apcu_store(
+            $item->getKey(),
+            serialize([$item->get(), $item->getExpirationTimestamp()]),
+            $ttl ?? 0
+        );
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function fetchItemFromCache($key)
-	{
-		$exists     = false;
-		$cachedItem = apcu_fetch($key, $exists);
+    /**
+     * {@inheritdoc}
+     */
+    protected function fetchItemFromCache($key)
+    {
+        $exists     = false;
+        $cachedItem = apcu_fetch($key, $exists);
 
-		if (!$exists) {
-			return [$exists, null, null];
-		}
+        if (!$exists) {
+            return [$exists, null, null];
+        }
 
-		$data = unserialize($cachedItem);
-		return [$exists, $data[0], $data[1]];
-	}
+        $data = unserialize($cachedItem);
+        return [$exists, $data[0], $data[1]];
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function removeAllItemFromCache()
-	{
-		return apcu_clear_cache();
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function removeAllItemFromCache()
+    {
+        return apcu_clear_cache();
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function removeItemFromCache($key)
-	{
-		apcu_delete($key);
-		return true;
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function removeItemFromCache($key)
+    {
+        apcu_delete($key);
+        return true;
+    }
 }
